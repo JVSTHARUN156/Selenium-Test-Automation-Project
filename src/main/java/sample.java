73c8978b756709c6ae1f9f99b2f498b1f6d32603
jvstharun156 <jvstharun369@gmail.com>
2026-06-08 10:30:22 +0530

    push

---
+++ src/main/java/sample.java
@@ -0,0 +1,19 @@
+public class sample {
+
+    //Syntax for a class
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!!");
+
+    }
+
+}
+/*
+Multi Line Comment
+
+{} - Denotes start and end of class/method/loop/condition
+[] - Used only for Arrays
+() - Parenthesis - Used for a method/loop/condition
+
+ */
---
+++ src/test/java/sample.java
@@ -0,0 +1,17 @@
+public class sample { a//Syntax for a class
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!!");
+
+    }
+
+}
+/*
+Multi Line Comment
+
+{} - Denotes start and end of class/method/loop/condition
+[] - Used only for Arrays
+() - Parenthesis - Used for a method/loop/condition
+
+ */
